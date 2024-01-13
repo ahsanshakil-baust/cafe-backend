41443ddb67c97ee0af669aaeee1ae83a2baa805f
ahsanshakil-baust <ahsanshakil.baust@gmail.com>
2024-01-13 12:07:23 +0600

    feat: login route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -38,7 +38,21 @@ router.post("/login", (req, res) => {
   const user = req.body;
   const query = "select email,password,role,status from user where email=?";
   connection.query(query,[user.email],(err,result)=>{
-    
+    if(!err){
+      if(result.length<=0 || result[0].password !==user.password){
+        return res.status(401).json({message:"Incorrect Username or Password"})
+      }
+      else if(result[0].status==="false"){
+        return res.status(401).json({message:"Wait for Admin Approval"})
+      }
+      else if(result[0].password===user.password){
+
+      }else{
+        result.status(400).json({message:"Something went wrong please try agin later."})
+      }
+    }else{
+      return res.status(500).json(err)
+    }
   })
 });
 
